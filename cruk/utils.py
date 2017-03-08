840df79ada721af5d0aa04dd38728dff2c0950df
Ubuntu <ubuntu@ip-172-31-3-221.us-west-1.compute.internal>
2017-03-08 16:57:33 +0000

    notebook with future payments model

---
+++ cruk/utils.py
@@ -0,0 +1,9 @@
+raw_pay_path = '/home/ubuntu/workspace/cruk_data/RAW_PAY_COMBINED_NEW2017-03-07 22-11-25.txt'
+reg_path_raw = '/home/ubuntu/workspace/cruk_data/RAW_REGISTRATIONS.csv'
+demo_path_raw = '/home/ubuntu/workspace/cruk_data/Supporter Demographics Final'
+
+
+
+pay_path = '/home/ubuntu/cdata/payments'
+reg_path = '/home/ubuntu/cdata/registrations'
+demo_path = '/home/ubuntu/cdata/demo'
